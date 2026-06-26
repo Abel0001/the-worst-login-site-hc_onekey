e98426d70f84170f0ea758552389753a2619b98c
Abel0001 <abel.words1@gmail.com>
2026-06-26 02:43:32 +0200

    Finished it

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 window.onload = onLoad
 let pointer = document.createElement("div")
 pointer.innerHTML += '<img src="pointer.png" alt="" width = 25>'
@@ -10,9 +11,8 @@ let shouldUseCaps = false
 
 let activeCharacterSetNum = 0
 let activeCharacterSet = [""]
-const CharacterSets = {"Latin character set": ["#", "@", "$", "%", "&", "*", "(", ")", "?", "!"],
-"Fake character set 1": [""],
-"Fake character set 2": [""]
+const CharacterSets = {"Numbers": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+"Basic Latin Letters": ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 };
 
 function onLoad(){
@@ -35,7 +35,8 @@ function understandControls(){
     document.body.innerHTML = '<h1>Controlling this mess</h1>\
     <h2>Press the "o" key to select stuff, or hold it if told(holding only registers after release)</h2>\
     <h2>You will have to enter your login information while a pointer moves over the characters<br>pressing "o" will select the character, while holding it will go to the password field.</h2>\
-    <h2>THe login information will be given just before the login screen appears, remember it well!\
+    <hr>\
+    <h2>The login information will be given just before the login screen appears, remember it well!\
     <h2>Press "o" to continue</h2>'
 
     pressAction = renderCapsNoCaps
@@ -71,14 +72,27 @@ function renderCharacterSet(){
         document.body.innerHTML += '<p>Press "o" to switch between options and hold to select</p>'
 }
 
+function randomStringFromCharSet(charSet, length){
+    let temp = ""
+    for (let i = 0; i < length; i++) {
+        temp += charSet[Math.floor(Math.random() * charSet.length)]
+        
+    }
+    return temp
+}
+
 
 let gameLoopInterval = null
 let currGridIndex = 0
-
+let correctUsername = ""
+let correctPsswd = ""
 function showLoginInfo(){
     holdAction = noAction
     pressAction =loginInfoPress
-    
+    correctUsername = randomStringFromCharSet(activeCharacterSet, 4)
+    correctPsswd = randomStringFromCharSet(activeCharacterSet, 4
+    )
+    document.body.innerHTML = '<div class="login-card"><h2>Friend\'s login info</h2><p>Username: '+ correctUsername + '</p><p>Password: '+correctPsswd+'</p></div>'
 }
 
 function renderGame(){
@@ -89,16 +103,18 @@ function renderGame(){
     
     currGridIndex = (currGridIndex + 1) % activeCharacterSet.length
     updateGameUI()
-}, 350)
+}, 380)
     updateGameUI()
 
 }
 let username = ""
 let password = ""
 let isEditingPassword = false
+let incorrectAttempt = false
 function updateGameUI(){
     let loginhtml = '<h1>Login Screen</h1>\
     <div>\
+    <h2 style="background-color: red;">'+(incorrectAttempt ? 'Incorrect username or password. Try again!': '')+'</h2>\
     <p><bold>Username: ' + username +' '+(!isEditingPassword ? '<span style="color:green">_</span>' : '')+'</p>\
     <p><bold>Password: ' + "*".repeat(password.length) +(isEditingPassword ? '<span style="color:green">_</span>' : '')+'</p>\
     </div>\
@@ -112,9 +128,9 @@ function updateGameUI(){
 }
 
 function renderLoggedIn(){
-    pressAction = noAction
+    pressAction = restart
     holdAction = noAction
-    document.body.innerHTML = '<h1>Welcome ' + username+ '!</h1><h2 style="margin:0">You have sucessfully logged in!</h2><h3>(or not, there\'s no database)<p>Your password is safe with us!'
+    document.body.innerHTML = '<h1>Welcome ' + username+ '!</h1><h2 style="margin:0">You have sucessfully logged in!</h2><p>Your password is safe with us!<hr><p>Press "o" to play again!'
 }
 
 
@@ -133,6 +149,13 @@ function characterSetSelectPress(){
 }
 function characterSetSelectHold(){
         activeCharacterSet = CharacterSets[Object.keys(CharacterSets)[activeCharacterSetNum]]
+        if(!shouldUseCaps){
+            for (let i = 0; i < activeCharacterSet.length; i++) {
+                activeCharacterSet[i] = activeCharacterSet[i].toLowerCase()
+                
+            }
+            activeCharacterSet = [...new Set(activeCharacterSet)]
+        }
         showLoginInfo()
 }
 function loginInfoPress(){
@@ -156,7 +179,16 @@ function gameHoldAction(){
         updateGameUI()
     }
     else{
+        if(password == correctPsswd && username == correctUsername){
         clearInterval(gameLoopInterval)
         renderLoggedIn()
+        }else{
+            incorrectAttempt = true
+            username = ""
+            password = ""
+            currGridIndex = 0
+            isEditingPassword = false
+        }
     }
 }
+function restart() {window.location.reload()}
